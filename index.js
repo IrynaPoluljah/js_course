97bfe3efa74aa0840c16d4664b852809c54fa6a9
IrynaPoluljah <pimpim2600@gmail.com>
2018-09-18 20:43:53 +0300

    lesson_9

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let surname = 'Полюлях';
+let name = 'Ирина';
+let fullName = surname + ' ' + name;
+console.log(fullName);
+
+
+
+
+let a = 19;
+let b = 8;
+
+a > b ? console.log(a) : console.log(b);
+
+a < b ? console.log(a) : console.log(b);
+
+console.log(a + b);
+
+console.log(a % b);
+
+if( a - b > 10 ) {
+  console.log( a + 10 )
+} else if ( a < b ) {
+  console.log(9);
+} else {
+  console.log(b);
+}
